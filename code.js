54ba80436f4e1d3b7db4cec8c019feb32c08df28
Rafal <czoki111@gmail.com>
2019-07-21 21:41:28 +0200

    Modification code.js

--- code.js
+++ code.js
@@ -1,1 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    box = document.querySelector('#box');
+})
 
+box.addEventListener('click', function(){
+    this.style.setProperty('background-color','red');
+    console.log('dziala click');
+})
