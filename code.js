ae2c7520b72ce0509de0656f885306c08514dd5f
Rafal <czoki111@gmail.com>
2019-07-23 23:10:09 +0200

    Tracking change in code.js

--- code.js
+++ code.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded', function(){
     box = document.querySelector('#box');
 })
 
-box.addEventListener('click', function(){
+box.addEventListener('click', function(e){
     this.style.setProperty('background-color','red');
     console.log('dziala click');
+    console.log('e: '+e);
+    console.log(e);
+    console.log('this: '+this);
+    console.log(this);
 })
